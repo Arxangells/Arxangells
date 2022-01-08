7190b9b31d79d027289baafd11937db1c38d7a21
liza <zoxon89@mail.ru>
2022-01-09 00:54:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,139 @@
+
+
+{/* <script src="script.js"></script> */}
+
+
+
+// console.log(2+2*2 !=9);                                  отрицание
+
+
+// const isCheked = true;
+// isClose = true;    
+//                                                          отрицание
+// console.log(isCheked && !isClose);
+
+
+
+// const isCheked = true;
+// isClose = true;                                          оператор или
+
+// console.log(isCheked || isClose);
+
+
+// const isCheked = true;                                   оператор и
+//       isClose = true;    
+
+// console.log(isCheked && isClose);
+
+
+// && - и 
+// || - или
+
+// let incr = 10, 
+// decr  = 10; 
+
+// console.log(incr++);                                     Постфиксная форма не изменна
+// console.log(--decr);                                     Префиксная форма уменшает и увеличивает на один
+
+
+// let incr = 10, 
+//     decr  = 10; 
+
+// ++incr; работает на плюс 1
+// --decr; работает на минус 1
+
+// console.log(incr);
+// console.log(decr);
+
+
+
+
+// console.log("arr" + "object");                           вывод в строку
+// console.log(4 + + "3");                                  вычисления в строке
+ 
+
+
+// const user = "ivan";
+// alert (`привет, ${user}`); вывод на любого юзера.. с учетом его перс данных.: `-` - одинарные скобки.
+
+// const category = "toys";
+// console.log("htts://someurl.com/category+4+toys" - `htts://someurl.com/${category}`); красивое написание
+
+
+// const answer = prompt("Вам есть 18?)", "18");
+// console.log(answer);                                     вопрос с ответом в консоли
+
+// const answers = [];                                      много вопросов
+// answers[0] = prompt ("Как Ваше имя?", "");
+// answers[1] = prompt ("Как Ваше слово?", "");
+// answers[2] = prompt ("Как Ваше хобби?", "");
+// answers[3] = prompt ("Как Ваше ничего?", "");
+
+// document.write (answers);                                выводит ответы на страницу.
+// console.log(typeof(answers)); -                          проверить на тип данных.
+// console.log(typeof(null)); -                             ошибка вывод типа данных.
+
+
+
+// const answer = prompt("Вам есть 18?)", "18");
+// console.log(typeof(answer)); тип строки
+
+// const answer = +prompt("Вам есть 18?)", "18");
+// console.log(typeof(answer));                             тип строки с расширением
+
+// const answer = prompt ("Вам есть 18?)", "18");           уже с ответом вопрос
+// console.log(answer + 5);                                 зачем добавил цифру.. в консоли
+
+// alert("hello");                                          вывод сообщения на страницу.
+
+// const result = confirm("are you here?");
+// console.log(result);                                     согласия или отрицание - удаленно от пользователя.
+
+// const Kalipso = {                                        обращение к чему либо в отдельности.
+//         name: "Liza",
+//         age: 36,
+//         ismarraied: false,   
+//         love_is_me: true};
+// // console.log(Kalipso.love_is_me);
+// !
+// console.log(Kalipso["love_is_me"]);          
+//              // 0           1               2
+// let arr = ["plum.png", "orange.jpn", "lion.pdf"]
+// console.log(arr[2]);                                     обращение в отдельности
+                
+// let und; 
+// console.log(und);            und.fined                   пустой контейнер
+//                              const kols = false;-        обман
+//                              true -                      правда
+
+// let number = 4.0; 
+//                              infinity-мат                ошибка
+// console.log (4/0);
+
+// "use struct"; -                                          строгий режим.
+// a = 15;
+// console.log(a);
+
+// {
+//     var result = "1"; видит let result = "1";            не видит в фигурных скобках
+// }
+// console.log(result);
+
+// var name = "ivan";
+//                                                          вывод имени
+// name = "alex";
+// console.log(name);
+
+// const kalipso = {
+//     a: 50
+//    };
+//                                                          вывод значения констансты
+//    kalipso.a = 14;
+
+// console.log(kalipso);
+
+// let number = 5;            let NUmber = 5; - другая переменная.
+// const LeftBorderWidth = 10;
+//                                                          Вывод значения
+// number = 5;
+// console.log(number);
